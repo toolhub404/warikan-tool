6aeaa61fbbad3aa044ea6195e7b217a40decc60a
workspace <hkt.44.hkt@gmail.com>
2026-05-01 20:09:04 +0900

    Implement service worker with caching strategy

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "warikan-tool-v11";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css?v=11",
+  "./script.js?v=11",
+  "./manifest.json?v=11"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
